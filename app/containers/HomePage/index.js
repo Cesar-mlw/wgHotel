3d23edafc44ac67a6427fec257639aa737bf32c2
Cesar Moura Leite Westphal <cesarmlwestphal@gmail.com>
2019-03-27 14:27:18 -0300

    Added action bar to the web page. Need to fix profice button error

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -1,3 +1,6 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable import/order */
+/* eslint-disable no-unused-vars */
 /*
  * HomePage
  *
@@ -10,16 +13,91 @@
  */
 
 import React from 'react';
-import { FormattedMessage } from 'react-intl';
-import messages from './messages';
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  IconButton,
+  MenuItem,
+  Menu,
+} from '@material-ui/core';
+import { AccountCircleOutlined, MenuOutlined } from '@material-ui/icons';
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+};
 
 /* eslint-disable react/prefer-stateless-function */
 export default class HomePage extends React.PureComponent {
+  state = {
+    anchorEl: null,
+  };
+
+  handleMenu = event => {
+    this.setState({ anchorEl: event.currentTarget });
+  };
+
+  handleClose = () => {
+    this.setState({ anchorEl: null });
+  };
+
   render() {
+    const { classes } = this.props;
+    const open = Boolean(this.state.anchorEl);
+    const anchorEl = this.state;
     return (
-      <h1>
-        <FormattedMessage {...messages.header} />
-      </h1>
+      <div className={styles.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              className={styles.menuButton}
+              color="inherit"
+              aria-label="Menu"
+            >
+              <MenuOutlined />
+            </IconButton>
+            <Typography variant="h6" color="inherit" className={styles.grow}>
+              wgHotel
+            </Typography>
+            <div>
+              <IconButton
+                aria-owns="menu-appbar"
+                aria-haspopup="true"
+                onClick={this.handleMenu}
+                color="inherit"
+              >
+                <AccountCircleOutlined />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={open}
+                onClose={this.handleClose}
+              >
+                <MenuItem onClick={this.handleClose}>Profile</MenuItem>
+                <MenuItem onClick={this.handleClose}>My Account</MenuItem>
+              </Menu>
+            </div>
+          </Toolbar>
+        </AppBar>
+      </div>
     );
   }
 }
